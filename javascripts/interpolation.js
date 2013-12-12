950ae511147aebd3f1b50a2e0537019fa2d86806
Chuphay <chuphay@gmail.com>
2013-12-12 17:00:35 +0800

    Update interpolation.js

--- javascripts/interpolation.js
+++ javascripts/interpolation.js
@@ -4,7 +4,7 @@ var context1 = my_canvas1.getContext("2d");
 
 //this code flips the y axis, so that it's not inverted 
 
-context1.transform(1,0,0,-1.0,0,150);
+context1.transform(1,0,0,-1.0,0,200);
 
 //this let's us draw to the canvas
 //a,b is the current position
@@ -22,9 +22,14 @@ function dot(screen,x,y,color){
  
    
 }
-dot(context1,10,10,"yellow");
-//this code let's us write to the html
 
+//this code let's us write to the html
+data = []; 
+for(var i = 0; i<10;i++){
+  var temp = 180*Math.random();
+  data.push(temp);
+  dot(context1,40*i,temp,"yellow");
+}
 function write(x){
     window.onload = function(){
         document.getElementById("console").innerHTML = x;
@@ -34,38 +39,3 @@ function write(x){
 
 
 write(["<br> Expected rate of return: <br>  <br>new line" ]);
-var currentX = 0;
-var currentY = 0;
-function steps(screen,n) {
-    currentX = 0;
-    currentY = 0;
-    var spacing = 1;
-    var otherSpacing =1;
-    for(var i=0;i<n;i++){
-        var rdm = 2*Math.random()-1;
-        
-        if (rdm>=0){
-            draw(screen,currentX,currentY,currentX + spacing,currentY +otherSpacing);
-            currentX = currentX + spacing;
-            currentY = currentY +otherSpacing;
-        }
-        else {
-            draw(screen,currentX,currentY,currentX + spacing,currentY -otherSpacing);
-            currentX = currentX + spacing;
-            currentY = currentY -otherSpacing;
-        }
-        
-    }
-}
-
-function monteCarlo(screen,m,n) {
-    for (var i = 0; i < m; i++) {
-        currentY = 0;
-        var color = "#ff"+i+"000";
-        screen.beginPath();
-        screen.strokeStyle = color;
-        steps(screen,n);
-        var index = i;   
-    }    
-}
-monteCarlo(context1,10,200);
